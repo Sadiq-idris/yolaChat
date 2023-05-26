1d3ac907b946343d377a117bfec37c7384f08e08
Assiddiq <106121876+Sadiq-idris@users.noreply.github.com>
2023-05-26 18:41:44 +0100

    Update settings.py

--- iyolachat/settings.py
+++ iyolachat/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-0ig021yc4wb@q9x9ny^ob#vv_8!msw%qqif)uu#n#h6&ne1l)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
